5002c3e99e2ca809ea7fa595f2d0b50ed13e931b
Ewan Jones <ewan@climacrux.com>
2025-11-04 22:12:29 +0100

    feat: add custom translations for OAuth grant request in i18n configuration

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,11 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().withCustomTranslations({
+    en: {
+        oauthGrantRequest: "{0} would like to access your data"
+    }
+}).build();
 
 type I18n = typeof ofTypeI18n;
 
